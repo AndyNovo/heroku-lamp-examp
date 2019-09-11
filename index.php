d6de2d2b524455c9976b94ead0ac2a194235ed83
Andy Novocin <andy@novocin.com>
2019-09-10 20:27:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    print_r("test");
 
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
